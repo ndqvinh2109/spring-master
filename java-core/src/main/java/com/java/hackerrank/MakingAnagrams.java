319ce73775f23a471cf697dff692ee7faab3b22f
Vinh Nguyen Dang Quang <vinh.nguyendang@axonactive.com>
2018-01-08 08:33:12 +0700

    Making Anagrams

---
+++ java-core/src/main/java/com/java/hackerrank/MakingAnagrams.java
@@ -0,0 +1,50 @@
+package com.java.hackerrank;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class MakingAnagrams {
+
+    /*
+    * For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
+    *
+    * */
+
+    public static int numberNeeded(String first, String second) {
+        Map<Character, Integer> occurenceMap = new HashMap<>();
+        int count = 0;
+
+
+        for(char firstC: first.toCharArray()) {
+            if( !occurenceMap.containsKey(firstC) ) {
+                occurenceMap.put(firstC, 1);
+            } else {
+                occurenceMap.put(firstC, occurenceMap.get(firstC) + 1);
+            }
+        }
+
+        for(char secondC: second.toCharArray()) {
+            if( !occurenceMap.containsKey(secondC) ) {
+                occurenceMap.put(secondC, 1);
+            } else {
+                occurenceMap.put(secondC, occurenceMap.get(secondC) + 1);
+            }
+        }
+
+        for(Map.Entry<Character,Integer> entry: occurenceMap.entrySet()){
+            if(entry.getValue() == 1){
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+        System.out.println(numberNeeded(a, b));
+    }
+}
